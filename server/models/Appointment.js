05496dd2a16b74f7f6d414fb7591138cda820dd7
mahmoudabdelaziz1993 <mahmoudabdelaziz1993@outlook.com>
2019-03-08 03:34:36 +0200

    setup end point for create,cancel,list  appointments

--- server/models/Appointment.js
+++ server/models/Appointment.js
@@ -3,6 +3,7 @@ const { Schema } = mongoose;
 const appointmentSchema = new Schema({
     doctor: { type: Schema.Types.ObjectId, ref: "User" },
     paitant: { type: Schema.Types.ObjectId, ref: "User" },
+    notes:String,
     date: Date
 });
 const Appointment = mongoose.model('appointments', appointmentSchema);
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/users');
+const appointmentController = require("../controllers/appointments");
 
 
 router.get('/current_user', (req, res) => res.send(req.user));
@@ -19,4 +20,22 @@ router.get("/reject_request/:requesterid",async (req,res)=>{
     const result = await controller.rejectRequest(req.params.requesterid,req.user);
     res.status(200).send(result);
 })
+// ----------------------------------------------------------------------------
+// -------------------------- Appointment section -----------------------------
+// ----------------------------------------------------------------------------
+// list  
+router.get("/appointment", async (req,res)=>{
+    const result = await appointmentController.listAppoinment(req.user);
+    res.send(result);
+});
+//create 
+router.get("/appointment/create/:withId", async (req,res)=>{
+    const result = await appointmentController.createAppointment(req.params.withId,req.body,req.user);
+    res.send(result);
+});
+// cancel 
+router.get("/appointment/cancel/:id", async (req,res)=>{
+    const result = await appointmentController.cancelAppoinment(req.params.id)
+    res.send(result);
+});
  module.exports = router
